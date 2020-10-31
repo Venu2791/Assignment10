e1b0b9d96b18a0665af7d8c88232c4e6deb00685
Venu2791 <42144197+Venu2791@users.noreply.github.com>
2020-10-31 13:56:38 +0530

    Update test_session10.py

--- test_session10.py
+++ test_session10.py
@@ -6,7 +6,6 @@ import inspect
 import re
 from datetime import datetime, date
 from  collections import namedtuple, Counter
-random_list = random.choices(range(1000), k=5)
 
 
 main_funcs = [func for func in inspect.getmembers(main) if inspect.isfunction(func[1])]
@@ -26,7 +25,7 @@ def test_readme_contents():
     readme_words = readme.read().split()
     readme.close()
     assert len(
-        readme_words) >= 300, "Make your README.md file interesting! Add atleast 300 words"
+        readme_words) >= 250, "Make your README.md file interesting! Add atleast 300 words"
 
 
 def test_readme_proper_description():
@@ -66,63 +65,30 @@ def test_function_name_had_cap_letter():
         assert len(re.findall(
             '([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
 
-num_profiles = 10
-nt_profiles = main.return_profiles_n_t(num_profiles)
-dict_profiles = main.return_n_dict_profiles(num_profiles)
+def test_dict_approach():
+    *_, = dict_approach()
+    assert len(_) > 1, "Returns all the calculation"
 
+def test_named_tuple():
+    *_, = named_tuple()
+    assert len(_) > 1, "Returns all the calculation"
 
-def test_get_nt_mc_blood_type():
-    counter = Counter()
-    for p in nt_profiles:
-        counter[p[1]] += 1
-    assert counter.most_common(1)[0][0] == main.get_nt_mc_blood_type(nt_profiles),\
-        'Check named tuple implementation of most common blood type'
-   
-def test_get_dict_mc_blood_type():
-    counter = Counter()
-    for key, profile in dict_profiles.items():
-        counter[profile.get('blood_type', '')] += 1
-    assert counter.most_common(1)[0][0] == main.get_dict_mc_blood_type(dict_profiles),\
-        'Check dict implementation of most common blood type'
+def test_named_tuple_vs_dict():
+    cnt = 0
+    for _ in range(10):
+        start1 = perf_counter()
+        *_, = dict_approach()
+        end1 = perf_counter()
+        elapsed1 = end1 - start1
 
-def test_get_nt_mean_loc():
-    mean_loc = (0,0)
-    num = len(nt_profiles._fields)
-    for p in nt_profiles:
-        mean_loc = mean_loc[0]+p[3][0],  mean_loc[1]+p[3][1]
-    mean_loc[0]/num, mean_loc[1]/num == main.get_nt_oldest_age(nt_profiles),\
-    'Check namedtuple implementation of mean location'
+        start2 = perf_counter()
+        *_, = named_tuple()
+        end2 = perf_counter()
+        elapsed2 = end2 - start2
 
-def test_get_dict_mean_loc():
-    mean_loc = (0,0)
-    num = len(dict_profiles)
-    for key, p in dict_profiles.items():
-        cur_loc = p.get('cur_loc', (0, 0))
-        mean_loc = mean_loc[0]+cur_loc[0],  mean_loc[1]+cur_loc[1] 
-    mean_loc[0]/num, mean_loc[1]/num == main.get_dict_mean_loc(dict_profiles),\
-    'Check dict implementation of mean location'      
-
-def test_get_dict_oldest_age():
-    profiles = [p for key, p in dict_profiles.items()]
-    max_age_prof = sorted(profiles, key=lambda x:x['age'])[-1]
-    assert max_age_prof['age'] == main.get_dict_oldest_age(dict_profiles),\
-    'Check dict implementation of max age'      
-
-def test_get_nt_oldest_age():
-    age = 0
-    age = max(p.age for p in nt_profiles)
-    assert age == main.get_nt_oldest_age(nt_profiles),\
-    'Check namedtuple implementation of max age'
-
-def test_get_nt_avg_age():
-    avg_age = sum(p.age for p in nt_profiles)/len(nt_profiles._fields)
-    assert avg_age == main.get_nt_avg_age(nt_profiles),\
-    'Check namedtuple implementation of avg. age'
-
-def test_get_dict_avg_age():
-    avg_age = sum(p['age'] for _, p in dict_profiles.items())/len(dict_profiles)
-    assert avg_age == main.get_dict_avg_age(dict_profiles),\
-    'Check dict implementation of avg. age'
+        if elapsed2 < elapsed1:
+            cnt+=1
+    assert cnt >= 4, "checking more than random times the namedtuple is performing better"
     
 def test_company_profile():
     profile = main.return_n_companies(1)[0]
